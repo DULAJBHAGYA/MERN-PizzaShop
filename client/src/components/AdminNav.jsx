7d145dc35d1ee63fa527254b92e7b2d33496ec79
Dulaj Bhagya Upananda <95066839+DULAJBHAGYA@users.noreply.github.com>
2024-01-16 23:53:52 +0530

    2nd commit January 16

--- client/src/components/AdminNav.jsx
+++ client/src/components/AdminNav.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { FaCartShopping, FaHeart } from "react-icons/fa6";
+import { MdLogout } from "react-icons/md";
 
 
 const AdminNav = () => {
@@ -10,11 +10,14 @@ const AdminNav = () => {
     }
 
     const navItems = [
-        {path:'/', title:'Home'},
-        {path:'/about', title:'About Us'},
-        {path:'/menu', title:'Menu'},
-        {path:'/promos', title:'Promotions'},
-        {path:'/contact', title:'Contact Us'},
+        {path:'/admin', title:'Dashboard'},
+        {path:'/admin/addcategory', title:'Add Category'},
+        {path:'/admin/addfood', title:'Add Food'},
+        {path:'/admin/users', title:'Users'},
+        {path:'/admin/orders', title:'Orders'},
+        {path:'/admin/deliveries', title:'Deliveries'},
+        {path:'/admin/feedback', title:'Feedbacks'},
+        {path:'/contact', title:'Settings'},
     ]
 
     
@@ -23,12 +26,12 @@ const AdminNav = () => {
     <header className='max-width-2xl position:sticky container mx-auto xl:px-24 px-4'>
         <nav className='flex justify-between items-center py-6'>
         <a href='/' className='flex  items-center gap-2 text-xl text-black'>
-        <img src='/images/Logo.png' alt='Description of the image' width={150}/></a>
+        <img src='/images/Logo.png' alt='Description of the image' width={100}/></a>
 
         <ul className='hidden md:flex gap-12'>
             {
                 navItems.map(({path, title}) =>(
-                    <li key={path} className='text-base text=l font-semibold text-black' style={{ fontFamily: 'Poppins' }}>
+                    <li key={path} className='text-base text=l font-semibold text-black' style={{ fontFamily: 'Inter' }}>
                     <NavLink
                     to={path}
                     className={({ isActive}) => isActive? "active": ""
@@ -42,14 +45,10 @@ const AdminNav = () => {
         </ul>
 
         <div className='flex'>
-        <div className='flex gap-10 mt-2'>
-        <Link to='/cart' ><FaHeart /></Link>
-        <Link to='/favorites' ><FaCartShopping/></Link>
+        <div className='flex gap-5 text-xl mt-2'>
+        <Link to='/cart' ><MdLogout /></Link>
         </div>
 
-        <div className='text-base text-black  font-medium space-x-5 hidden lg:block px-10 '>
-            <Link to='/login' className='py-2 px-5 border-2 border-black rounded-2xl'>Register / Login</Link>
-        </div>
         </div>
 
         </nav>      
