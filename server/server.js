d4ed81f5efb74bef7dfd5f09c7e0be0820663290
Dulaj Bhagya Upananda <95066839+DULAJBHAGYA@users.noreply.github.com>
2024-01-24 22:39:28 +0530

    24th January

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//import routes
+const categoryRoutes = require('./routes/category');
+
+//app middleware
+app.use(bodyParser.json());
+
+app.use(categoryRoutes);
+
+
+const PORT = 8000;
+
+const DB_URL = 'mongodb+srv://dulajupananda:Ddbu1998@cluster0.wwprcvx.mongodb.net/';
+
+mongoose.connect(DB_URL)
+.then(() =>{
+    console.log('DB connected');
+})
+.catch((err) =>{
+    console.log('DB connection error', err);
+})
+
+app.listen(PORT, () =>{
+    console.log(`App is running on ${PORT}`);
+});
+
+
